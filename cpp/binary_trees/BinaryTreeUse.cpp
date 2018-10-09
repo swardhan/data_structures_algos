960507e6ab19f29c333782975a299b7441de9b51
saumya <saumyawardhan1@gmail.com>
2018-10-09 12:56:52 +0530

    Sum of Binary Tree Nodes

--- cpp/binary_trees/BinaryTreeUse.cpp
+++ cpp/binary_trees/BinaryTreeUse.cpp
@@ -183,6 +183,41 @@ int diameter(BinaryTreeNode<int>* root){
 	return max(option1, max(option2, option3));
 }
 
+//Better DiameterOf A Binary Tree
+pair<int, int> heightDiameter(BinaryTreeNode<int>* root){
+	if(root == NULL){
+		pair<int, int> p;
+		p.first = 0;
+		p.second = 0;
+		return p;
+	}
+
+	pair<int, int> leftAns = heightDiameter(root->left);
+	pair<int, int> rightAns = heightDiameter(root->right);
+	int ld = leftAns.second;
+	int lh = leftAns.first;
+	int rd = rightAns.second;
+	int rh = rightAns.first;
+
+	int height = 1 + max(lh, rh);
+	int diameter = max(lh+rh, max(ld, rd));
+	pair<int, int> p;
+	p.first = height;
+	p.second = diameter;
+	return p;
+}
+
+int sumOfNodes(BinaryTreeNode<int>* root){
+	if(root == NULL){
+		return 0;
+	}
+
+	if(!root->left && !root->right){
+		return root->data;
+	}
+	return root->data + sumOfNodes(root->left) + sumOfNodes(root->right);
+}
+
 //Sample Tree: 1 2 3 4 5 6 7 -1 -1 -1 -1  8 9 -1 -1 -1 -1 -1 -1
 
 int main(){
@@ -196,10 +231,16 @@ int main(){
 
 	//Take Input Level Wise
 	BinaryTreeNode<int>* root = takeInputLevelWise();
-	// printTreeLevelWise(root);
+	// printTreeLevelWigitse(root);
 	printTree(root);
 
-	cout << "Diameter : " << diameter(root) << endl;
+	// //Sum of Nodes
+	// cout << "Sum Of Nodes: " << sumOfNodes(root) << endl;
+
+	// //Diameter of a Tree
+	// pair<int, int> p = heightDiameter(root);
+	// cout << "Diameter : " << p.second << endl;
+	// cout << "Height : " << p.first << endl;
 
 	//Traversals
 	// cout << "inOrder: " << inOrder(root) << endl;
